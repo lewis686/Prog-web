d80599dfb5033079244d6bd7dbfe4a3b979afaa4
Butnariu Luis <alexandruluis2006@gmail.com>
2026-02-25 10:23:04 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    const nume = document.getElementById("un_name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("mesaj").value;
+
+    console.warn( "Goodbye World!");
+}
